726cb31508e7d7f26fb1ef9c5973d744ddc7b7d5
Vjaceslavs Lukasevics <vjaceslavs.lukasevics@printify.com>
2022-04-08 17:57:44 +0200

    Revive the setup and adjust to support arm64

--- docker/stop-local-jenkins.sh
+++ docker/stop-local-jenkins.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-docker-compose -f jenkins-docker-compose.yml down --rmi all -v --remove-orphans
+docker-compose down --rmi all -v --remove-orphans
 echo "Stoping local jenkins..."
